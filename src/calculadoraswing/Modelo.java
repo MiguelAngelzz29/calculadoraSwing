103cb40fdeb4447cf2790e654029499cb74e7eeb
MiguelAngelzz29 <miguelzz29@gmail.com>
2022-11-05 12:26:30 +0100

    first commit

---
+++ src/calculadoraswing/Modelo.java
@@ -0,0 +1,35 @@
+package calculadoraswing;
+
+public class Modelo {
+
+    
+    private double resultado =0;
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+    
+    public double sumar (double operador) {
+         return resultado += operador;
+   
+        }
+    
+    public double restar (double operador){
+        return resultado -= operador;
+    }
+    
+    public double multiplicar (double operador){
+        return resultado *= operador;
+    }
+    
+    public double dividir (double operador){
+        return resultado /= operador;
+    }
+    
+  
+    }
+
